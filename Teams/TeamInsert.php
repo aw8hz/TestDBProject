457ecfd78c9810d2988c6b062e264e049a109a71
Andrew <aw8hz@virginia.edu>
2021-05-02 17:10:03 -0400

    Add prepared statements for teams

--- Teams/TeamInsert.php
+++ Teams/TeamInsert.php
@@ -9,13 +9,26 @@ if (mysqli_connect_errno()){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$sql_insert = "INSERT INTO team (name, home_city, wins, losses, overtime_losses, conference, division, coach, general_manager, stadium) VALUES ('$_POST[name]','$_POST[home_city]','$_POST[wins]','$_POST[losses]','$_POST[overtime_losses]','$_POST[conference]','$_POST[division]','$_POST[coach]','$_POST[general_manager]','$_POST[stadium]')";
+$sql_insert = $db_con->prepare("INSERT INTO team (name, home_city, wins, losses, overtime_losses, conference, division, coach, general_manager, stadium) VALUES (?,?,?,?,?,?,?,?,?,?)");
 
-if (!mysqli_query($db_con,$sql_insert)){
-	die('Error: ' . mysqli_error($db_con));
-}
+$sql_insert->bind_param("ssiiisssss", $name, $home_city, $wins, $losses, $overtime_losses, $conference, $division, $coach, $general_manager, $stadium);
+
+$name = $_POST['name'];
+$home_city = $_POST['home_city'];
+$wins = $_POST['wins'];
+$losses = $_POST['losses'];
+$overtime_losses = $_POST['overtime_losses'];
+$conference = $_POST['conference'];
+$division = $_POST['division'];
+$coach = $_POST['coach'];
+$general_manager = $_POST['general_manager'];
+$stadium = $_POST['stadium'];
+
+$sql_insert->execute();
 
 echo "1 record added";
+
+$sql_insert->close();
 
 mysqli_close($db_con);
 ?>
--- Teams/TeamUpdate.php
+++ Teams/TeamUpdate.php
@@ -9,26 +9,40 @@ if (mysqli_connect_errno()){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$sql_update = "UPDATE team SET ";
-$sql_update .= ($_POST['home_city'] ? "home_city = '" . $_POST['home_city']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['games_played']) ? ", " : "") . ($_POST['games_played'] ? "games_played = '" . $_POST['games_played']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['wins']) ? ", " : "") . ($_POST['wins'] ? "wins = '" . $_POST['wins']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['losses']) ? ", " : "") . ($_POST['losses'] ? "losses = '" . $_POST['losses']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['overtime_losses']) ? ", " : "") . ($_POST['overtime_losses'] ? "overtime_losses = '" . $_POST['overtime_losses']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['conference']) ? ", " : "") . ($_POST['conference'] ? "conference = '" . $_POST['conference']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['division']) ? ", " : "") . ($_POST['division'] ? "division = '" . $_POST['division']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['coach']) ? ", " : "") . ($_POST['coach'] ? "coach = '" . $_POST['coach']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['general_manager']) ? ", " : "") . ($_POST['general_manager'] ? "general_manager = '" . $_POST['general_manager']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['stadium']) ? ", " : "") . ($_POST['stadium'] ? "stadium = '" . $_POST['stadium']."'" : "");
-$sql_update .= " WHERE name = '" . $_POST['name']."';";
+$row_select = "SELECT * FROM team WHERE name='" . $_POST['name'] . "';";
 
-//echo $sql_update;
+echo $row_select;
 
-if (!mysqli_query($db_con,$sql_update)){
-	die('Error: ' . mysqli_error($db_con));
+$curr_row = mysqli_fetch_array(mysqli_query($db_con, $row_select));
+
+print_r($curr_row);
+
+print_r($_POST);
+
+if (!$curr_row){
+	die('error: ' . mysqli_error($db_con));
 }
 
+$sql_update = $db_con->prepare("UPDATE team SET home_city=?, wins=?, losses=?, overtime_losses=?, conference=?, division=?, coach=?, general_manager=?, stadium=? WHERE name=?");
+
+$sql_update->bind_param("siiissssss", $home_city, $wins, $losses, $overtime_losses, $conference, $division, $coach, $general_manager, $stadium, $name);
+
+$home_city = ($_POST['home_city'] ?: $curr_row['home_city']);
+$wins = ($_POST['wins'] ?: $curr_row['wins']);
+$losses = ($_POST['losses'] ?: $curr_row['losses']);
+$overtime_losses = ($_POST['overtime_losses'] ?: $curr_row['overtime_losses']);
+$conference = ($_POST['conference'] ?: $curr_row['conference']);
+$division = ($_POST['division'] ?: $curr_row['division']);
+$coach = ($_POST['coach'] ?: $curr_row['coach']);
+$general_manager = ($_POST['general_manager'] ?: $curr_row['general_manager']);
+$stadium = ($_POST['stadium'] ?: $curr_row['stadium']);
+$name = ($_POST['name'] ?: $curr_row['name']);
+
+$sql_update->execute();
+
 echo "1 record updated";
+
+$sql_update->close();
 
 mysqli_close($db_con);
 ?>
