5748b3c3ee6ea19a0d1304258a7a1111bdd8e22d
Andrew <aw8hz@virginia.edu>
2021-05-02 18:23:35 -0400

    fix game form and add prepared statements

--- Games/GameUpdate.php
+++ Games/GameUpdate.php
@@ -9,30 +9,49 @@ if (mysqli_connect_errno()){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$sql_update = "UPDATE game SET ";
-$sql_update .= ($_POST['OT'] ? "OT = '" . $_POST['OT']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['shootout']) ? ", " : "") . ($_POST['shootout'] ? "shootout = '" . $_POST['shootout']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['home_team']) ? ", " : "") . ($_POST['home_team'] ? "home_team = '" . $_POST['home_team']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['away_team']) ? ", " : "") . ($_POST['away_team'] ? "away_team = '" . $_POST['away_team']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['home_goals']) ? ", " : "") . ($_POST['home_goals'] ? "home_goals = '" . $_POST['home_goals']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['home_PIM']) ? ", " : "") . ($_POST['home_PIM'] ? "home_PIM = '" . $_POST['home_PIM']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['home_corsi']) ? ", " : "") . ($_POST['home_corsi'] ? "home_corsi = '" . $_POST['home_corsi']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['home_shots_on_net']) ? ", " : "") . ($_POST['home_shots_on_net'] ? "home_shots_on_net = '" . $_POST['home_shots_on_net']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['home_missed_shots']) ? ", " : "") . ($_POST['home_missed_shots'] ? "home_missed_shots = '" . $_POST['home_missed_shots']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['away_goals']) ? ", " : "") . ($_POST['away_goals'] ? "away_goals = '" . $_POST['away_goals']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['away_PIM']) ? ", " : "") . ($_POST['away_PIM'] ? "away_PIM = '" . $_POST['away_PIM']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['away_corsi']) ? ", " : "") . ($_POST['away_corsi'] ? "away_corsi = '" . $_POST['away_corsi']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['away_shots_on_net']) ? ", " : "") . ($_POST['away_shots_on_net'] ? "away_shots_on_net = '" . $_POST['away_shots_on_net']."'" : "");
-$sql_update .= (((strlen($sql_update) > 16) and $_POST['away_missed_shots']) ? ", " : "") . ($_POST['away_missed_shots'] ? "away_missed_shots = '" . $_POST['away_missed_shots']."'" : "");
-$sql_update .= " WHERE number = '" . $_POST['number']."';";
+$row_select = "SELECT * FROM game WHERE number='" . $_POST['number'] . "';";
 
-//echo $sql_update;
+//echo $row_select;
 
-if (!mysqli_query($db_con,$sql_update)){
+$curr_row = mysqli_fetch_array(mysqli_query($db_con, $row_select));
+
+//print_r($curr_row);
+
+//print_r($_POST);
+
+if (!$curr_row){
 	die('Error: ' . mysqli_error($db_con));
 }
 
-echo "1 record updated";
+$sql_update = $db_con->prepare("UPDATE game SET OT=?, shootout=?, home_team=?, away_team=?, home_goals=?, home_PIM=?, home_corsi=?, home_shots_on_net=?, home_missed_shots=?, away_goals=?, away_PIM=?, away_corsi=?, away_shots_on_net=?, away_missed_shots=? WHERE number=?");
 
+$sql_update->bind_param("iissiiiiiiiiiii", $OT, $shootout, $home_team, $away_team, $home_goals, $home_PIM, $home_corsi, $home_shots_on_net, $home_missed_shots, $away_goals, $away_PIM, $away_corsi, $away_shots_on_net, $away_missed_shots, $number);
+
+$OT = (strlen($_POST['OT']) ? $_POST['OT'] : $curr_row['OT']);
+$shootout = (strlen($_POST['shootout']) ? $_POST['shootout'] : $curr_row['shootout']);
+$home_team = ($_POST['home_team'] ?: $curr_row['home_team']);
+$away_team = ($_POST['away_team'] ?: $curr_row['away_team']);
+$home_goals = (strlen($_POST['home_goals']) ? $_POST['home_goals'] : $curr_row['home_goals']);
+$home_PIM = (strlen($_POST['home_PIM']) ? $_POST['home_PIM'] : $curr_row['home_PIM']);
+$home_corsi = (strlen($_POST['home_corsi']) ? $_POST['home_corsi'] : $curr_row['home_corsi']);
+$home_shots_on_net = (strlen($_POST['home_shots_on_net']) ? $_POST['home_shots_on_net'] : $curr_row['home_shots_on_net']);
+$home_missed_shots = (strlen($_POST['home_missed_shots']) ? $_POST['home_missed_shots'] : $curr_row['home_missed_shots']);
+$away_goals = (strlen($_POST['away_goals']) ? $_POST['away_goals'] : $curr_row['away_goals']);
+$away_PIM = (strlen($_POST['away_PIM']) ? $_POST['away_PIM'] : $curr_row['away_PIM']);
+$away_corsi = (strlen($_POST['away_corsi']) ? $_POST['away_corsi'] : $curr_row['away_corsi']);
+$away_shots_on_net = (strlen($_POST['away_shots_on_net']) ? $_POST['away_shots_on_net'] : $curr_row['away_shots_on_net']);
+$away_missed_shots = (strlen($_POST['away_missed_shots']) ? $_POST['away_missed_shots'] : $curr_row['away_missed_shots']);
+$number = ($_POST['number'] ?: $curr_row['number']);
+
+//echo $sql_update;
+
+if (!$sql_update->execute()){
+	echo "Update failed.";
+	echo mysqli_error($db_con);
+}else{
+	echo "1 record updated";
+}
+
+$sql_update->close();
 mysqli_close($db_con);
 ?>
