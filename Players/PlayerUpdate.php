de1c8d73e22aeba4f9d7eb2cdf2153ccbc06387a
Andrew <aw8hz@virginia.edu>
2021-05-02 16:49:40 -0400

    add prepared statements for skaters

--- Players/PlayerUpdate.php
+++ Players/PlayerUpdate.php
@@ -9,27 +9,42 @@ if (mysqli_connect_errno()){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$sql_update = "UPDATE skater SET ";
-$sql_update .= ($_POST['number'] ? "number = '" . $_POST['number']."'" : "");
-$sql_update .= (((strlen($sql_update) > 19) and $_POST['position']) ? ", " : "") . ($_POST['position'] ? "position = '" . $_POST['position']."'" : "");
-$sql_update .= (((strlen($sql_update) > 19) and $_POST['team']) ? ", " : "") . ($_POST['team'] ? "team = '" . $_POST['team']."'" : "");
-$sql_update .= (((strlen($sql_update) > 19) and $_POST['goals']) ? ", " : "") . ($_POST['goals'] ? "goals = '" . $_POST['goals']."'" : "");
-$sql_update .= (((strlen($sql_update) > 19) and $_POST['assists']) ? ", " : "") . ($_POST['assists'] ? "assists = '" . $_POST['assists']."'" : "");
-$sql_update .= (((strlen($sql_update) > 19) and $_POST['plus_minus']) ? ", " : "") . ($_POST['plus_minus'] ? "plus_minus = '" . $_POST['plus_minus']."'" : "");
-$sql_update .= (((strlen($sql_update) > 19) and $_POST['salary']) ? ", " : "") . ($_POST['salary'] ? "salary = '" . $_POST['salary']."'" : "");
-$sql_update .= (((strlen($sql_update) > 19) and $_POST['handedness']) ? ", " : "") . ($_POST['handedness'] ? "handedness = '" . $_POST['handedness']."'" : "");
-$sql_update .= (((strlen($sql_update) > 19) and $_POST['home_town']) ? ", " : "") . ($_POST['home_town'] ? "home_town = '" . $_POST['home_town']."'" : "");
-$sql_update .= (((strlen($sql_update) > 19) and $_POST['games_played']) ? ", " : "") . ($_POST['games_played'] ? "games_played = '" . $_POST['games_played']."'" : "");
-$sql_update .= (((strlen($sql_update) > 19) and $_POST['line_name']) ? ", " : "") . ($_POST['line_name'] ? "line_name = '" . $_POST['line_name']."'" : "");
-$sql_update .= " WHERE name = '" . $_POST['name']."';";
+$row_select = "SELECT * FROM skater WHERE name='" . $_POST['name'] . "';";
 
-//echo $sql_update;
+//echo $row_select;
 
-if (!mysqli_query($db_con,$sql_update)){
+$curr_row = mysqli_fetch_array(mysqli_query($db_con, $row_select));
+
+//print_r($curr_row);
+
+//print_r($_POST);
+
+if (!$curr_row){
 	die('Error: ' . mysqli_error($db_con));
 }
 
+$sql_update = $db_con->prepare("UPDATE skater SET number=?, position=?, team=?, goals=?, assists=?, plus_minus=?, salary=?, handedness=?, home_town=?, games_played=?, line_name=? WHERE name=?");
+
+$sql_update->bind_param("issiiiississ", $number, $position, $team, $goals, $assists, $plus_minus, $salary, $handedness, $home_town, $games_played, $line_name, $name);
+
+$number = ($_POST['number'] ?: $curr_row['number']);
+$position = ($_POST['position'] ?: $curr_row['position']);
+$team = ($_POST['team'] ?: $curr_row['team']);
+$goals = ($_POST['goals'] ?: $curr_row['goals']);
+$assists = ($_POST['assists'] ?: $curr_row['assists']);
+$plus_minus = ($_POST['plus_minus'] ?: $curr_row['plus_minus']);
+$salary = ($_POST['salary'] ?: $curr_row['salary']);
+$handedness = ($_POST['handedness'] ?: $curr_row['handedness']);
+$home_town = ($_POST['home_town'] ?: $curr_row['home_town']);
+$games_played = ($_POST['games_played'] ?: $curr_row['games_played']);
+$line_name = ($_POST['line_name'] ?: $curr_row['line_name']);
+$name = ($_POST['name'] ?: $curr_row['name']);
+
+$sql_update->execute();
+
 echo "1 record updated";
+
+$sql_update->close();
 
 mysqli_close($db_con);
 ?>
